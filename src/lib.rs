c675edc271ccb313361632b8a61c7c2130811581
Ollivier Robert <roberto@keltia.net>
2021-10-12 11:57:32 +0200

    Fix module naming issues.

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 //!
 //! Examples:
 //! ```
-//!   use crate::leftpad_rs::*;
+//!   use leftpad_rs::{pad,pad_char};
 //!
 //!   let r = pad("foo", 5);  // -> "  foo"
 //!
@@ -13,66 +13,61 @@
 //! ```
 
 /// This module implements `pad` and `pad_char`.
-pub mod leftpad_rs {
-
-    /// Left-pads the string with spaces.
-    pub fn pad(s: &str, n: usize) -> String {
-        format!("{:>width$}", s, width = n)
-    }
-
-    /// Left-pads the string with the supplied character.
-    pub fn pad_char(s: &str, n: usize, c: char) -> Result<String, &str> {
-        let l = s.len();
-
-        if n == 0 {
-            return Err("invalid size");
-        }
-        if n <= l {
-            return Ok(s.to_string());
-        }
-        let f = c.to_string().repeat(n - l);
-        Ok(format!("{}{}", f, s))
-    }
-
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-
-        use rstest::rstest;
-
-        #[rstest]
-        #[case(2, "foo")]
-        #[case(3, "foo")]
-        #[case(4, " foo")]
-        #[case(5, "  foo")]
-        fn test_pad(#[case] n: usize, #[case] want: &str) {
-            assert_eq!(want, pad("foo", n));
-        }
-
-        #[test]
-        fn test_nopad() {
-            assert_ne!(pad("foo", 6), "foobar")
-        }
-
-        #[rstest]
-        #[case(2, "foo")]
-        #[case(3, "foo")]
-        #[case(4, "Xfoo")]
-        #[case(5, "XXfoo")]
-        fn test_pad_char(#[case] n: usize, #[case] want: &str) {
-            assert_eq!(Ok(want.to_string()), pad_char("foo", n, 'X'));
-        }
-
-        #[test]
-        fn test_pad_char_0() {
-            assert_eq!(Err("invalid size"), pad_char("foo", 0, 'X'))
-        }
-
-        #[test]
-        fn test_nopad_char() {
-            assert_ne!(Ok("foobar".to_string()), pad_char("foo", 6, 'X'))
-        }
-    }
+/// Left-pads the string with spaces.
+pub fn pad(s: &str, n: usize) -> String {
+    format!("{:>width$}", s, width = n)
 }
 
-//pub use crate::leftpad_rs::*;
+/// Left-pads the string with the supplied character.
+pub fn pad_char(s: &str, n: usize, c: char) -> Result<String, &str> {
+    let l = s.len();
+
+    if n == 0 {
+        return Err("invalid size");
+    }
+    if n <= l {
+        return Ok(s.to_string());
+    }
+    let f = c.to_string().repeat(n - l);
+    Ok(format!("{}{}", f, s))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(2, "foo")]
+    #[case(3, "foo")]
+    #[case(4, " foo")]
+    #[case(5, "  foo")]
+    fn test_pad(#[case] n: usize, #[case] want: &str) {
+        assert_eq!(want, pad("foo", n));
+    }
+
+    #[test]
+    fn test_nopad() {
+        assert_ne!(pad("foo", 6), "foobar")
+    }
+
+    #[rstest]
+    #[case(2, "foo")]
+    #[case(3, "foo")]
+    #[case(4, "Xfoo")]
+    #[case(5, "XXfoo")]
+    fn test_pad_char(#[case] n: usize, #[case] want: &str) {
+        assert_eq!(Ok(want.to_string()), pad_char("foo", n, 'X'));
+    }
+
+    #[test]
+    fn test_pad_char_0() {
+        assert_eq!(Err("invalid size"), pad_char("foo", 0, 'X'))
+    }
+
+    #[test]
+    fn test_nopad_char() {
+        assert_ne!(Ok("foobar".to_string()), pad_char("foo", 6, 'X'))
+    }
+}
